b2a703db77ecd7203a946331944a2c67e55ef088
dapo bello <dapobello98@gmail.com>
2021-12-17 15:47:34 -0800

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+
 import { Redirect, Route } from 'react-router-dom';
 import {
   IonApp,
@@ -9,11 +10,14 @@ import {
   IonTabs,
   setupIonicReact
 } from '@ionic/react';
+
 import { IonReactRouter } from '@ionic/react-router';
-import { ellipse, square, triangle } from 'ionicons/icons';
+//import { ellipse, square, triangle } from 'ionicons/icons';
+import { images, square, triangle } from 'ionicons/icons';
 import Tab1 from './pages/Tab1';
 import Tab2 from './pages/Tab2';
 import Tab3 from './pages/Tab3';
+
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -33,6 +37,7 @@ import '@ionic/react/css/display.css';
 
 /* Theme variables */
 import './theme/variables.css';
+
 
 setupIonicReact();
 
@@ -60,9 +65,9 @@ const App: React.FC = () => (
             <IonLabel>Tab 1</IonLabel>
           </IonTabButton>
           <IonTabButton tab="tab2" href="/tab2">
-            <IonIcon icon={ellipse} />
-            <IonLabel>Tab 2</IonLabel>
-          </IonTabButton>
+  <IonIcon icon={images} />
+  <IonLabel>Photos</IonLabel>
+</IonTabButton>
           <IonTabButton tab="tab3" href="/tab3">
             <IonIcon icon={square} />
             <IonLabel>Tab 3</IonLabel>
